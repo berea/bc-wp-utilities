29621c458f3afff67a06463aa6ec5be48c1b20a0
Mark Ross <rossma@berea.edu>
2015-06-19 16:53:24 -0400

    Added the git clone if not already cloned.

--- 99gulp.sh
+++ 99gulp.sh
@@ -13,6 +13,8 @@ fi
 
 if [ ! -f /usr/local/bin/sass ]; then
 	su -c "gem install sass"
+	cp /usr/local/bin/sass /usr/bin
+	cp /usr/local/bin/scss /usr/bin	
 fi
 
 cd /var/www/wordpress/wp-content/themes/bc-wp-2015
@@ -20,6 +22,10 @@ cd /var/www/wordpress/wp-content/themes/bc-wp-2015
 #checks to see if the devDependencies are installed
 if [ ! -d node_modules/ ]; then
 	npm install --save-dev
+fi
+
+if [ ! -d /var/www/wordpress/wp-content/themes/bc-wp-2015/ ]; then
+	git clone https://github.com/berea/bc-wp-2015
 fi
 
 gulp
